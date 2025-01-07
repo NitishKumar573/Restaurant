fda16ad34a1e3b1beff25acbd11b5e7ec64bcc4b
NitishKumar573 <mk7497665@gmail.com>
2025-01-07 18:24:16 +0530

    adding files

---
+++ java.js
@@ -0,0 +1,15 @@
+const searchIcon = document.querySelector(".search-icon");
+const searchContainer = document.querySelector(".search-container");
+
+searchIcon.addEventListener("click", () => {
+    searchContainer.classList.toggle("active");
+});
+function openSide(){
+    const hellos = document.querySelector(".yetam");
+    hellos.style.display = 'flex';
+
+}
+
+function closeSide(){
+    hellos.style.display='none';
+}
